3c317ec2e4fcec02758c6e2638d2f372f1c42f7b
wtfplayer <136761546+wtfplayer@users.noreply.github.com>
2023-10-26 19:14:21 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+document.getElementById('Discord').onclick = function() {
+    window.open('https://discord.gg/gyYTywnpug', '_blank');
+}
+document.getElementById('githSource').onclick = function() {
+    window.open('https://github.com/abhisheknaiiduh/revs-admin/blob/main/universal%20admin', '_blank')
+}
+document.getElementById('Youtube').onclick = function() {
+    window.open('https://youtube.com/@normalexploiter123', '_blank');
+}
+document.querySelector("#Loadstring").onclick = function() {
+    navigator.clipboard.writeText(document.querySelector("#Loadstring").textContent);
+}
